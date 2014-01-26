362cf533399efcff02818992c46ec0c72189b8fe
Doug Hellmann <doug@doughellmann.com>
2014-01-26 12:35:03 -0500

    use .format()
    Change-Id: I9dc0216c9b32cfb88b10155caa8af96a82be4895

--- source/atexit/atexit_multiple.py
+++ source/atexit/atexit_multiple.py
@@ -32,7 +32,7 @@ import atexit
 
 
 def my_cleanup(name):
-    print('my_cleanup(%s)' % name)
+    print('my_cleanup({})'.format(name))
 
 atexit.register(my_cleanup, 'first')
 atexit.register(my_cleanup, 'second')
