55c625fdf49d4f6416ce1f64bcd9ef2fcddf14a9
Doug Hellmann <doug@doughellmann.com>
2015-12-26 10:55:13 -0500

    asyncio: change protocol client to support multiple clients

--- source/asyncio/asyncio_echo_client_protocol.py
+++ source/asyncio/asyncio_echo_client_protocol.py
@@ -16,30 +16,35 @@ class EchoClient(asyncio.Protocol):
     def __init__(self, messages, loop):
         self.messages = messages
         self.loop = loop
-        self._received_len = 0
-        self._sent_len = 0
 
     def connection_made(self, transport):
+        self.transport = transport
         self.address = transport.get_extra_info('peername')
         print('connecting to {} port {}'.format(*self.address),
               file=sys.stderr)
+        # This could be transport.writelines() except that
+        # would make it harder to show each part of the message
+        # being sent.
         for msg in self.messages:
             transport.write(msg)
             print('{}: sending {!r}'.format(self.address, msg),
                   file=sys.stderr)
-            self._sent_len += len(msg)
+        if transport.can_write_eof():
+            transport.write_eof()
 
     def data_received(self, data):
         print('{}: received {!r}'.format(self.address, data),
               file=sys.stderr)
-        self._received_len += len(data)
-        if self._received_len == self._sent_len:
-            self.loop.stop()
+
+    def eof_received(self):
+        print('{}: received EOF'.format(self.address),
+              file=sys.stderr)
+        self.transport.close()
 
     def connection_lost(self, exc):
-        print('{}: server closed connection'.format(self.address),
-              file=sys.stderr)
-        self.loop.stop()
+        print('{}: server closed connection'.format(
+            self.address), file=sys.stderr)
+        self.transport.close()
 
 
 messages = [
@@ -50,8 +55,21 @@ messages = [
 server_address = ('localhost', 10000)
 
 event_loop = asyncio.get_event_loop()
-client = functools.partial(EchoClient, messages, event_loop)
-coroutine = event_loop.create_connection(client, *server_address)
-event_loop.run_until_complete(coroutine)
-event_loop.run_forever()
+
+client_factory = functools.partial(EchoClient,
+                                   messages,
+                                   event_loop)
+connection_factory = functools.partial(
+    event_loop.create_connection,
+    client_factory,
+    *server_address,
+)
+event_loop.run_until_complete(
+    asyncio.wait(
+        [connection_factory(),
+         connection_factory()],
+        loop=event_loop,
+    )
+)
+
 event_loop.close()
