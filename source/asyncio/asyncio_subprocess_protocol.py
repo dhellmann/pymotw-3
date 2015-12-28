39766b17d46281082296db9714e936a1b47c7513
Doug Hellmann <doug@doughellmann.com>
2015-12-28 15:17:28 -0500

    asyncio: add error handling to df protocol

--- source/asyncio/asyncio_subprocess_protocol.py
+++ source/asyncio/asyncio_subprocess_protocol.py
@@ -25,21 +25,33 @@ class DFProtocol(asyncio.SubprocessProtocol):
         self.done = done
         self.output = bytearray()
 
+    def connection_made(self, transport):
+        LOG.debug('process started {}'.format(transport.get_pid()))
+        self.transport = transport
+
     def pipe_data_received(self, fd, data):
         LOG.debug('read {} bytes'.format(len(data)))
         self.output.extend(data)
 
     def process_exited(self):
-        LOG.debug('process exited, parsing output')
-        cmd_output = bytes(self.output).decode()
-        results = self._parse_results(cmd_output)
-        self.done.set_result(results)
+        LOG.debug('process exited')
+        return_code = self.transport.get_returncode()
+        LOG.debug('return code {}'.format(return_code))
+        if not return_code:
+            cmd_output = bytes(self.output).decode()
+            results = self._parse_results(cmd_output)
+        else:
+            results = []
+        self.done.set_result((return_code, results))
 
     def _parse_results(self, output):
+        LOG.debug('parsing results')
         # Output has one row of headers, all single words.  The
         # remaining rows are one per filesystem, with columns more or
         # less matching the headers (assuming that none of the mount
         # points have whitespace in the names).
+        if not output:
+            return []
         lines = output.splitlines()
         headers = lines[0].split()
         devices = lines[1:]
@@ -55,13 +67,19 @@ async def run_df(loop):
 
     cmd_done = asyncio.Future(loop=loop)
     factory = functools.partial(DFProtocol, cmd_done)
-    create = loop.subprocess_exec(
+    proc = loop.subprocess_exec(
         factory,
         'df', '-hl',
-        stdin=None, stderr=None)
-    transport, protocol = await create
-    await cmd_done
-    transport.close()
+        stdin=None,
+        stderr=None,
+    )
+    try:
+        LOG.debug('launching process')
+        transport, protocol = await proc
+        LOG.debug('waiting for process to complete')
+        await cmd_done
+    finally:
+        transport.close()
 
     return cmd_done.result()
 
@@ -70,11 +88,14 @@ event_loop = asyncio.get_event_loop()
 
 try:
     LOG.debug('entering event loop')
-    result = event_loop.run_until_complete(run_df(event_loop))
+    return_code, results = event_loop.run_until_complete(run_df(event_loop))
 finally:
     LOG.debug('closing event loop')
     event_loop.close()
 
-LOG.debug('Free space:')
-for r in result:
-    LOG.debug('{Mounted:25}: {Avail}'.format(**r))
+if return_code:
+    LOG.debug('error exit {}'.format(return_code))
+else:
+    print('\nFree space:')
+    for r in results:
+        print('{Mounted:25}: {Avail}'.format(**r))
