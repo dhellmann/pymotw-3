fb2eaddd17b531c6961f6374d2002921128667b3
Doug Hellmann <doug@doughellmann.com>
2016-01-24 12:51:30 -0500

    gettext: turn of flake8 checking for app_builtin example
    the _() function installed into the global namespace throws off flake8

--- source/gettext/gettext_app_builtin.py
+++ source/gettext/gettext_app_builtin.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 #
 # Copyright (c) 2009 Doug Hellmann All rights reserved.
 #
 """
 """
+# flake8: noqa
 #end_pymotw_header
 
 import gettext
 gettext.install('gettext_example', 'locale',
                 unicode=True, names=['ngettext'])
 
-print _('This message is in the script.')
+print(_('This message is in the script.'))
