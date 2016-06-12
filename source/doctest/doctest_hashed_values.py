21383c2958898aa42294a961d2e6e8327a726290
Doug Hellmann <doug@doughellmann.com>
2016-06-12 10:56:12 -0400

    doctest: simplify hash example

--- source/doctest/doctest_hashed_values.py
+++ source/doctest/doctest_hashed_values.py
@@ -9,17 +9,5 @@
 
 keys = ['a', 'aa', 'aaa']
 
-d1 = {k: len(k) for k in keys}
-d2 = {k: len(k) for k in reversed(keys)}
-
-print('d1:', d1)
-print('d2:', d2)
-print('d1 == d2:', d1 == d2)
-
-s1 = set(keys)
-s2 = set(reversed(keys))
-
-print()
-print('s1:', s1)
-print('s2:', s2)
-print('s1 == s2:', s1 == s2)
+print('dict:', {k: len(k) for k in keys})
+print('set :', set(keys))
