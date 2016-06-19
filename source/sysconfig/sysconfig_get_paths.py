b95f8c2329fe607634668b599bb5602aadf3bd79
Doug Hellmann <doug@doughellmann.com>
2016-06-18 21:01:08 -0400

    sysconfig: use format()

--- source/sysconfig/sysconfig_get_paths.py
+++ source/sysconfig/sysconfig_get_paths.py
@@ -16,7 +16,7 @@ for scheme in ['posix_prefix', 'posix_user']:
     print('=' * len(scheme))
     paths = sysconfig.get_paths(scheme=scheme)
     prefix = os.path.commonprefix(paths.values())
-    print('prefix = %s\n' % prefix)
+    print('prefix = {}\n'.format(prefix))
     for name, path in sorted(paths.items()):
-        print('%s\n  .%s' % (name, path[len(prefix):]))
+        print('{}\n  .{}'.format(name, path[len(prefix):]))
     print()
