273396006f0b3229b86310ec6f4a65e5b86325fc
Doug Hellmann <doug@doughellmann.com>
2016-06-18 21:06:59 -0400

    shelve: pep8

--- source/shelve/shelve_create.py
+++ source/shelve/shelve_create.py
@@ -6,4 +6,8 @@
 import shelve
 
 with shelve.open('test_shelf.db') as s:
-    s['key1'] = { 'int': 10, 'float':9.5, 'string':'Sample data' }
+    s['key1'] = {
+        'int': 10,
+        'float': 9.5,
+        'string': 'Sample data',
+    }
