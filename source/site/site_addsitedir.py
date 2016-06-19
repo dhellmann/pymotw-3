f0753597f53f5b4cf54517f4f98ccb6010d9cb64
Doug Hellmann <doug@doughellmann.com>
2016-06-18 22:01:16 -0400

    site: flake8

--- source/site/site_addsitedir.py
+++ source/site/site_addsitedir.py
@@ -3,6 +3,8 @@
 #
 # Copyright (c) 2010 Doug Hellmann.  All rights reserved.
 #
+# Ignore flake8 error about redefined mymodule
+# flake8: noqa
 """Add site import directory containing regular python modules.
 """
 #end_pymotw_header
@@ -16,15 +18,15 @@ module_directory = os.path.join(script_directory, sys.argv[1])
 
 try:
     import mymodule
-except ImportError, err:
-    print 'Could not import mymodule:', err
+except ImportError as err:
+    print('Could not import mymodule:', err)
 
 print()
 before_len = len(sys.path)
 site.addsitedir(module_directory)
 print('New paths:')
 for p in sys.path[before_len:]:
-    print(p.replace(os.getcwd(), '.')) # shorten dirname
+    print(p.replace(os.getcwd(), '.'))  # shorten dirname
 
 print()
 import mymodule
--- source/site/with_modules/mymodule.py
+++ source/site/with_modules/mymodule.py
@@ -8,4 +8,6 @@
 #end_pymotw_header
 
 import os
-print('Loaded', __name__, 'from', __file__[len(os.getcwd())+1:])
+print('Loaded {} from {}'.format(
+    __name__, __file__[len(os.getcwd()) + 1:])
+)
