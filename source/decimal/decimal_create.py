d91ace10438983ab5738e86b2aaa0b6c068a1e59
Doug Hellmann <doug@doughellmann.com>
2016-08-14 14:48:43 -0400

    decimal: don't use %

--- source/decimal/decimal_create.py
+++ source/decimal/decimal_create.py
@@ -10,6 +10,7 @@
 import decimal
 
 fmt = '{0:<25} {1:<25}'
+
 print(fmt.format('Input', 'Output'))
 print(fmt.format('-' * 25, '-' * 25))
 
@@ -22,5 +23,7 @@ print(fmt.format('3.14', decimal.Decimal('3.14')))
 # Float
 f = 0.1
 print(fmt.format(repr(f), decimal.Decimal(str(f))))
-print(fmt.format('%.23g' % f,
-                 str(decimal.Decimal.from_float(f))[:25]))
+print('{:<0.23g} {:<25}'.format(
+    f,
+    str(decimal.Decimal.from_float(f))[:25])
+)
