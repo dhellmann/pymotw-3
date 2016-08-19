0de5b8cfad584c0e9b920aa170180383786052f1
Doug Hellmann <doug@doughellmann.com>
2016-08-19 16:38:54 -0400

    tarfile: fix bytes in addfile_string

--- source/tarfile/tarfile_addfile_string.py
+++ source/tarfile/tarfile_addfile_string.py
@@ -11,12 +11,13 @@
 import io
 import tarfile
 
-data = 'This is the data to write to the archive.'
+text = 'This is the data to write to the archive.'
+data = text.encode('utf-8')
 
 with tarfile.open('addfile_string.tar', mode='w') as out:
     info = tarfile.TarInfo('made_up_file.txt')
     info.size = len(data)
-    out.addfile(info, io.BytesIO(data.encode('utf-8')))
+    out.addfile(info, io.BytesIO(data))
 
 print('Contents:')
 with tarfile.open('addfile_string.tar', mode='r') as t:
