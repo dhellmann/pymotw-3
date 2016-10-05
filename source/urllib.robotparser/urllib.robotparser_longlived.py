8b05ef95c5bb69c903ab78e42fa23c72cf453c9b
Doug Hellmann <doug@doughellmann.com>
2016-10-05 18:00:08 -0400

    urllib.robotparser: print & pep8

--- source/urllib.robotparser/urllib.robotparser_longlived.py
+++ source/urllib.robotparser/urllib.robotparser_longlived.py
@@ -23,19 +23,19 @@ PATHS = [
     '/PyMOTW/',
     '/admin/',
     '/downloads/PyMOTW-1.92.tar.gz',
-    ]
+]
 
 for path in PATHS:
     age = int(time.time() - parser.mtime())
-    print 'age:', age,
+    print('age:', age, end=' ')
     if age > 1:
-        print 'rereading robots.txt'
+        print('rereading robots.txt')
         parser.read()
         parser.modified()
     else:
-        print
-    print '%6s : %s' % (parser.can_fetch(AGENT_NAME, path), path)
+        print()
+    print('{:6} : {}'.format(
+        parser.can_fetch(AGENT_NAME, path), path))
     # Simulate a delay in processing
     time.sleep(1)
-    print
-    
+    print()
--- source/urllib.robotparser/urllib.robotparser_simple.py
+++ source/urllib.robotparser/urllib.robotparser_simple.py
@@ -21,10 +21,12 @@ PATHS = [
     '/PyMOTW/',
     '/admin/',
     '/downloads/PyMOTW-1.92.tar.gz',
-    ]
+]
 
 for path in PATHS:
-    print '%6s : %s' % (parser.can_fetch(AGENT_NAME, path), path)
+    print('{:6} : {}'.format(
+        parser.can_fetch(AGENT_NAME, path), path))
     url = urlparse.urljoin(URL_BASE, path)
-    print '%6s : %s' % (parser.can_fetch(AGENT_NAME, url), url)
-    print
+    print('{:6} : {}'.format(
+        parser.can_fetch(AGENT_NAME, url), url))
+    print()
