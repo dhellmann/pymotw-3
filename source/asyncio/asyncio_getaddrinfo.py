75cdd83a858d7247c004430857116bd20e8b93dd
Doug Hellmann <doug@doughellmann.com>
2016-11-13 11:30:31 -0500

    asyncio: clean up dns examples

--- source/asyncio/asyncio_getaddrinfo.py
+++ source/asyncio/asyncio_getaddrinfo.py
@@ -13,23 +13,26 @@ import socket
 import sys
 
 
-targets = [
+TARGETS = [
     ('pymotw.com', 'https'),
     ('doughellmann.com', 'https'),
     ('python.org', 'https'),
 ]
 
-event_loop = asyncio.get_event_loop()
-try:
+
+async def main(loop, targets):
     for target in targets:
-        info = event_loop.run_until_complete(
-            event_loop.getaddrinfo(
-                *target,
-                proto=socket.IPPROTO_TCP,
-            )
+        info = await event_loop.getaddrinfo(
+            *target,
+            proto=socket.IPPROTO_TCP,
         )
 
         for host in info:
             print('{:20}: {}'.format(target[0], host[4][0]))
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    event_loop.run_until_complete(main(event_loop, TARGETS))
 finally:
     event_loop.close()
--- source/asyncio/asyncio_getnameinfo.py
+++ source/asyncio/asyncio_getnameinfo.py
@@ -13,18 +13,20 @@ import socket
 import sys
 
 
-targets = [
+TARGETS = [
     ('66.33.211.242', 443),
     ('104.130.43.121', 443),
 ]
 
+
+async def main(loop, targets):
+    for target in targets:
+        info = await event_loop.getnameinfo(target)
+        print('{:15}: {} {}'.format(target[0], *info))
+
+
 event_loop = asyncio.get_event_loop()
 try:
-    for target in targets:
-        info = event_loop.run_until_complete(
-            event_loop.getnameinfo(target)
-        )
-
-        print('{:15}: {} {}'.format(target[0], *info))
+    event_loop.run_until_complete(main(event_loop, TARGETS))
 finally:
     event_loop.close()
