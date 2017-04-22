012eafcd7be4185f8dad2dad7fb9593cb02af6ee
Doug Hellmann <doug@doughellmann.com>
2017-04-22 14:10:54 -0400

    inspect: fix line wrap width

--- source/inspect/inspect_currentframe.py
+++ source/inspect/inspect_currentframe.py
@@ -11,7 +11,8 @@ def recurse(limit, keyword='default', *, kwonly='must be named'):
     local_variable = '.' * limit
     keyword = 'changed value of argument'
     frame = inspect.currentframe()
-    print('line {} of {}'.format(frame.f_lineno, frame.f_code.co_filename))
+    print('line {} of {}'.format(frame.f_lineno,
+                                 frame.f_code.co_filename))
     print('locals:')
     pprint.pprint(frame.f_locals)
     print()
