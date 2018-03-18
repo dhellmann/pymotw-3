e1a293cf3c5ae7305ad5bd42c576f54463d2ef98
Doug Hellmann <doug@doughellmann.com>
2018-03-18 16:01:10 -0400

    sysconfig: convert dict values to list before indexing
    commonprefix() tries to index into the argument it is given, and the
    view returned by dict.values() does not support indexing.

--- source/sysconfig/sysconfig_get_paths.py
+++ source/sysconfig/sysconfig_get_paths.py
@@ -15,7 +15,7 @@ for scheme in ['posix_prefix', 'posix_user']:
     print(scheme)
     print('=' * len(scheme))
     paths = sysconfig.get_paths(scheme=scheme)
-    prefix = os.path.commonprefix(paths.values())
+    prefix = os.path.commonprefix(list(paths.values()))
     print('prefix = {}\n'.format(prefix))
     for name, path in sorted(paths.items()):
         print('{}\n  .{}'.format(name, path[len(prefix):]))
