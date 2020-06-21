7b2992715b3b774f6d1e8ed2d727d23d892770aa
Doug Hellmann <doug@doughellmann.com>
2020-06-21 18:02:16 -0400

    concurrent.futures: protect concurrent use of functions in main module
    Protect multiprocessing use of functions in the main module by adding
    a check for `__main__`.
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- source/concurrent.futures/futures_process_pool_broken.py
+++ source/concurrent.futures/futures_process_pool_broken.py
@@ -13,17 +13,18 @@ import os
 import signal
 
 
-with futures.ProcessPoolExecutor(max_workers=2) as ex:
-    print('getting the pid for one worker')
-    f1 = ex.submit(os.getpid)
-    pid1 = f1.result()
+if __name__ == '__main__':
+    with futures.ProcessPoolExecutor(max_workers=2) as ex:
+        print('getting the pid for one worker')
+        f1 = ex.submit(os.getpid)
+        pid1 = f1.result()
 
-    print('killing process {}'.format(pid1))
-    os.kill(pid1, signal.SIGHUP)
+        print('killing process {}'.format(pid1))
+        os.kill(pid1, signal.SIGHUP)
 
-    print('submitting another task')
-    f2 = ex.submit(os.getpid)
-    try:
-        pid2 = f2.result()
-    except futures.process.BrokenProcessPool as e:
-        print('could not start new tasks: {}'.format(e))
+        print('submitting another task')
+        f2 = ex.submit(os.getpid)
+        try:
+            pid2 = f2.result()
+        except futures.process.BrokenProcessPool as e:
+            print('could not start new tasks: {}'.format(e))
--- source/concurrent.futures/futures_process_pool_map.py
+++ source/concurrent.futures/futures_process_pool_map.py
@@ -16,7 +16,8 @@ def task(n):
     return (n, os.getpid())
 
 
-ex = futures.ProcessPoolExecutor(max_workers=2)
-results = ex.map(task, range(5, 0, -1))
-for n, pid in results:
-    print('ran task {} in process {}'.format(n, pid))
+if __name__ == '__main__':
+    ex = futures.ProcessPoolExecutor(max_workers=2)
+    results = ex.map(task, range(5, 0, -1))
+    for n, pid in results:
+        print('ran task {} in process {}'.format(n, pid))
